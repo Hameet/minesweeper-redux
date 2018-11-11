a7ef37b1e4016be54e2d92c6f66002d4d4d4ca21
Hameet <hameet.singh@hotmail.com>
2018-11-11 13:54:47 +1300

    Added board and dependencies

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import './App.css'
+
+import Cell from './Cell'
+
+const Board = styled.div`
+align-self: center;
+display: grid;
+grid-area: board;
+grid-gap: 0;
+grid-template-areas: 'zero one two' 'three four five' 'six seven eight';
+grid-template-columns: 20vh 20vh 20vh;
+grid-template-rows: 20vh 20vh 20vh;
+height: 60vh;
+justify-self: center;
+margin: auto;
+width: 60vh;
+color: black;
+border-style: dotted;
+`
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <Board>
+          <Cell />
+          Hey
+        </Board>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Cell.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Cell = styled.div`
+color: green;
+  font-size: 2rem;
+  width: 75px;
+  height: 75px;
+  border: 1px #ccc dotted;
+  border-radius: 10%;
+  background-color: white;
+  margin: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;`
+
+export default Cell
